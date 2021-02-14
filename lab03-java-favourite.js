af5720f013af9ae5dc369c8964b67b59f1d090b6
KSKSIN <77713987+KwanKSK@users.noreply.github.com>
2021-02-14 19:41:25 +0700

    lab03-1

---
+++ lab03-java-favourite.js
@@ -0,0 +1,6 @@
+var hero=prompt("What is your charactor?");
+var color=prompt("What is the main color of " + hero + " ?");
+
+console.log(hero + " is your favorite charator.");
+console.log(hero + " is " + color +".");
+
